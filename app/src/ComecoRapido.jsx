b587191394da1eba1cc0c150903403b040bf706d
EdiJunior88 <er.moura88@yahoo.com.br>
2023-03-07 18:27:26 -0300

    Exibindo dados

--- app/src/ComecoRapido.jsx
+++ app/src/ComecoRapido.jsx
@@ -1,31 +1,23 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./ComecoRapido.css"
-import Button from "react-bootstrap/Button";
-import avatar from "./Imagem/avatar.png"
+import "./ComecoRapido.css";
 
-function MeuBotao() {
-  return <Button variant='primary'>Eu sou um botão</Button>;
-}
-
-function PaginaSobre() {
-  return (
-    <>
-      <h1>Sobre</h1>
-      <p>
-        Olá aqui. <br />
-        Como vai você?
-      </p>
-    </>
-  );
-}
+const usuario = {
+  nome: "Edivaldo Junior",
+  imagemUrl: "https://avatars.githubusercontent.com/u/13875907?v=4",
+  imagemTamanho: 90,
+};
 
 export default function MyApp() {
   return (
     <>
-      <h1>Bem-vindo ao meu app</h1>
-      <MeuBotao />
-      <img className="avatar" src={avatar} alt="avatar"/>
+      <h1>{usuario.nome}</h1>
+      <img
+        className='avatar'
+        src={usuario.imagemUrl}
+        alt={"Avatar do " + usuario.nome}
+        style={{ width: usuario.imagemTamanho, height: usuario.imagemTamanho }}
+      />
     </>
   );
 }
