0055c42568381a27834501847f17cd5097827e22
EdiJunior88 <er.moura88@yahoo.com.br>
2023-03-07 18:13:26 -0300

    Escrevendo marcação com JSX

--- app/src/ComecoRapido.jsx
+++ app/src/ComecoRapido.jsx
@@ -2,18 +2,26 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from 'react-bootstrap/Button';
 
-
 function MeuBotao() {
   return (
     <Button variant='primary'>Eu sou um botão</Button>
   );
 }
 
+function PaginaSobre() {
+  return (
+    <>
+      <h1>Sobre</h1>
+      <p>Olá aqui. <br />Como vai você?</p>
+    </>
+  )
+}
+
 export default function MyApp() {
   return (
-    <div>
+    <>
       <h1>Bem-vindo ao meu app</h1>
       <MeuBotao />
-    </div>
+    </>
   );
 }
