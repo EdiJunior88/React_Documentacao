cdd25bbd46ae2b30bf7c7ff7e329e827511d8742
EdiJunior88 <er.moura88@yahoo.com.br>
2023-03-07 18:21:10 -0300

    Adicionando estilos

--- app/src/ComecoRapido.jsx
+++ app/src/ComecoRapido.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Button from 'react-bootstrap/Button';
+import "./ComecoRapido.css"
+import Button from "react-bootstrap/Button";
+import avatar from "./Imagem/avatar.png"
 
 function MeuBotao() {
-  return (
-    <Button variant='primary'>Eu sou um botão</Button>
-  );
+  return <Button variant='primary'>Eu sou um botão</Button>;
 }
 
 function PaginaSobre() {
   return (
     <>
       <h1>Sobre</h1>
-      <p>Olá aqui. <br />Como vai você?</p>
+      <p>
+        Olá aqui. <br />
+        Como vai você?
+      </p>
     </>
-  )
+  );
 }
 
 export default function MyApp() {
@@ -22,6 +25,7 @@ export default function MyApp() {
     <>
       <h1>Bem-vindo ao meu app</h1>
       <MeuBotao />
+      <img className="avatar" src={avatar} alt="avatar"/>
     </>
   );
 }
