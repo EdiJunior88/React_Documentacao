eee95485a6a84cd472306bf23990aa73016d3e72
EdiJunior88 <er.moura88@yahoo.com.br>
2023-03-08 20:50:00 -0300

    renderização condicional

--- app/src/ComecoRapido.jsx
+++ app/src/ComecoRapido.jsx
@@ -8,16 +8,53 @@ const usuario = {
   imagemTamanho: 90,
 };
 
-export default function MyApp() {
+function PainelAdministrador() {
   return (
-    <>
-      <h1>{usuario.nome}</h1>
-      <img
-        className='avatar'
-        src={usuario.imagemUrl}
-        alt={"Avatar do " + usuario.nome}
-        style={{ width: usuario.imagemTamanho, height: usuario.imagemTamanho }}
-      />
-    </>
+    <div>
+      <h1>Painel de Administrador</h1>
+      <p>Está logado</p>
+    </div>
   );
+}
+
+function FormularioLogin() {
+  return (
+    <form>
+      <h1>Formulario de Login</h1>
+      <label htmlFor='nome'>Nome: </label>
+      <input type='text' name='nome' />
+    </form>
+  );
+}
+
+export default function MyApp() {
+  //Em vez disso, você usará as mesmas técnicas usadas
+  //ao escrever código JavaScript regular. Por exemplo,
+  //você pode usar uma if instrução para incluir JSX condicionalmente:
+  // let conteudo;
+  // let estaLogadoEm = true;
+
+  // if (estaLogadoEm) {
+  //   conteudo = <PainelAdministrador />;
+  // } else {
+  //   conteudo = <FormularioLogin />;
+  // }
+
+  // return <>{conteudo}</>;
+
+  //----------------------------------------------
+
+  //Se você preferir um código mais compacto,
+  //pode usar o operador condicional ?.
+  //Ao contrário do if, ele funciona dentro do JSX:
+
+  // let estaLogadoEm = true;
+  // return (
+  //   <div>{estaLogadoEm ? <PainelAdministrador /> : <FormularioLogin />}</div>
+  // );
+
+  //Quando você não precisa da elseramificação,
+  //também pode usar uma sintaxe lógica&& mais curta:
+  let estaLogadoEm = true;
+  return <div>{estaLogadoEm && <PainelAdministrador />}</div>;
 }
