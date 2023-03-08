19b68ba22dbd30b138fd9e96fe9bc5b39aee3de8
EdiJunior88 <er.moura88@yahoo.com.br>
2023-03-08 20:57:41 -0300

    Listas de renderização

--- app/src/ComecoRapido.jsx
+++ app/src/ComecoRapido.jsx
@@ -2,59 +2,16 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./ComecoRapido.css";
 
-const usuario = {
-  nome: "Edivaldo Junior",
-  imagemUrl: "https://avatars.githubusercontent.com/u/13875907?v=4",
-  imagemTamanho: 90,
-};
-
-function PainelAdministrador() {
-  return (
-    <div>
-      <h1>Painel de Administrador</h1>
-      <p>Está logado</p>
-    </div>
-  );
-}
-
-function FormularioLogin() {
-  return (
-    <form>
-      <h1>Formulario de Login</h1>
-      <label htmlFor='nome'>Nome: </label>
-      <input type='text' name='nome' />
-    </form>
-  );
-}
+const produtos = [
+  { titulo: "Repolho", fruta: false, id: 1 },
+  { titulo: "Alho", fruta: false, id: 2 },
+  { titulo: "Maçã", fruta: true, id: 3 },
+];
 
 export default function MyApp() {
-  //Em vez disso, você usará as mesmas técnicas usadas
-  //ao escrever código JavaScript regular. Por exemplo,
-  //você pode usar uma if instrução para incluir JSX condicionalmente:
-  // let conteudo;
-  // let estaLogadoEm = true;
+  const listaItens = produtos.map((produto) => (
+    <li key={produto.id} style={{color: produto.fruta ? "magenta" : "darkgreen"}}>{produto.titulo}</li>
+  ));
 
-  // if (estaLogadoEm) {
-  //   conteudo = <PainelAdministrador />;
-  // } else {
-  //   conteudo = <FormularioLogin />;
-  // }
-
-  // return <>{conteudo}</>;
-
-  //----------------------------------------------
-
-  //Se você preferir um código mais compacto,
-  //pode usar o operador condicional ?.
-  //Ao contrário do if, ele funciona dentro do JSX:
-
-  // let estaLogadoEm = true;
-  // return (
-  //   <div>{estaLogadoEm ? <PainelAdministrador /> : <FormularioLogin />}</div>
-  // );
-
-  //Quando você não precisa da elseramificação,
-  //também pode usar uma sintaxe lógica&& mais curta:
-  let estaLogadoEm = true;
-  return <div>{estaLogadoEm && <PainelAdministrador />}</div>;
+  return <ul>{listaItens}</ul>;
 }
