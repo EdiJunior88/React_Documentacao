820c8b771afba6564d014f73e9ea9578389c2de4
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-13 18:35:56 -0300

    - Começo rápido Criando e aninhando componentes Escrevendo marcação com JSX

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,12 @@
 import MyButton from "./Components/MyButton";
+import AboutPage from "./Pages/AboutPage";
 
 const App = () => {
   return (
     <div>
       <h1>Bem vindo ao meu app</h1>
       <MyButton />
+      <AboutPage />
     </div>
   );
 };
---
+++ app/src/Pages/AboutPage.jsx
@@ -0,0 +1,10 @@
+const AboutPage = () => {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Olá, ei... aqui.<br />Como você está?</p>
+    </>
+  )
+}
+
+export default AboutPage
