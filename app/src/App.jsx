c93d7b126783c274f601c60136da04c6d19f8bda
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-13 18:32:56 -0300

    - Começo rápido Criando e aninhando componentes

---
+++ app/src/App.jsx
@@ -0,0 +1,12 @@
+import MyButton from "./Components/MyButton";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Bem vindo ao meu app</h1>
+      <MyButton />
+    </div>
+  );
+};
+
+export default App;
---
+++ app/src/Components/MyButton.jsx
@@ -0,0 +1,5 @@
+const MyButton = () => {
+  return <button>Eu sou um botão</button>;
+};
+
+export default MyButton;
