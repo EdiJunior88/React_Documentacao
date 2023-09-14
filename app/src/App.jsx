363ade9e453b83521ec7f6d4f0c2f39c2d23895e
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-13 21:15:35 -0300

    - Começo rápido Criando e aninhando componentes Escrevendo marcação com JSX Adicionando estilos Exibindo dados Renderização condicional Respondendo a eventos Atualizando a tela Usando ganchos Compartilhando dados entre componentes

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,22 +1,22 @@
 import { useState } from "react";
 
 export default function App() {
-  return (
-    <div>
-      <h1>A contagem sofre alterações separadas</h1>
-      <MeuBotao />
-      <br />
-      <MeuBotao />
-    </div>
-  );
-}
-
-function MeuBotao() {
   const [contagem, setContagem] = useState(0);
 
   function contagemClique() {
     setContagem(contagem + 1);
   }
 
-  return <button onClick={contagemClique}>Você clicou {contagem}x</button>;
+  return (
+    <div>
+      <h1>A contagem sofre alterações separadas</h1>
+      <MeuBotao contagem={contagem} onClick={contagemClique} />
+      <br />
+      <MeuBotao contagem={contagem} onClick={contagemClique} />
+    </div>
+  );
+}
+
+function MeuBotao({ contagem, onClick }) {
+  return <button onClick={onClick}>Você clicou {contagem}x</button>;
 }
